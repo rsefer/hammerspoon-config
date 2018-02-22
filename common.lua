c242b07bdee4b12221e2de3bf75b43e7e2f0a3bc
Robert Sefer <rsefer@gmail.com>
2018-02-22 13:35:06 -0600

    Better window gridding on launch

--- common.lua
+++ common.lua
@@ -9,46 +9,44 @@ function file_exists(name)
 end
 
 function gridset(x1, y1, w1, h1, nickname)
-  return function()
-    local currentwin = hs.window.focusedWindow()
-    local currentRect = hs.grid.get(currentwin)
-    local win = hs.window.focusedWindow()
-    local monitorName = win:screen():name()
-    if nickname ~= nil and monitorName == 'DELL P2415Q' then
-      if nickname == 'three-quarters' then
-        x1 = 25
-      elseif nickname == 'one-quarter' then
-        x1 = 0
-      end
+  local currentwin = hs.window.focusedWindow()
+  local currentRect = hs.grid.get(currentwin)
+  local win = hs.window.focusedWindow()
+  local monitorName = win:screen():name()
+  if nickname ~= nil and monitorName == 'DELL P2415Q' then
+    if nickname == 'three-quarters' then
+      x1 = 25
+    elseif nickname == 'one-quarter' then
+      x1 = 0
     end
-    if x1 == 'current' then
-      x2 = currentRect.x
-    elseif x1 == 'opp' then
-      x2 = 100 - currentRect.w
-    else
-      x2 = x1
-    end
-    if y1 == 'current' then
-      y2 = currentRect.y
-    else
-      y2 = y1
-    end
-    if w1 == 'current' then
-      w2 = currentRect.w
-    else
-      w2 = w1
-    end
-    if h1 == 'current' then
-      h2 = currentRect.h
-    else
-      h2 = h1
-    end
-    hs.grid.set(
-      win,
-      { x = x2, y = y2, w = w2, h = h2 },
-      win:screen()
-    )
   end
+  if x1 == 'current' then
+    x2 = currentRect.x
+  elseif x1 == 'opp' then
+    x2 = 100 - currentRect.w
+  else
+    x2 = x1
+  end
+  if y1 == 'current' then
+    y2 = currentRect.y
+  else
+    y2 = y1
+  end
+  if w1 == 'current' then
+    w2 = currentRect.w
+  else
+    w2 = w1
+  end
+  if h1 == 'current' then
+    h2 = currentRect.h
+  else
+    h2 = h1
+  end
+  hs.grid.set(
+    win,
+    { x = x2, y = y2, w = w2, h = h2 },
+    win:screen()
+  )
 end
 
 -- https://stackoverflow.com/a/10992898
--- windows.lua
+++ windows.lua
@@ -10,29 +10,46 @@ require 'common'
 
 local windowGridKeyCombo = {'cmd', 'alt', 'ctrl'}
 
--- Size Left Half
-hs.hotkey.bind(windowGridKeyCombo, 'L', gridset(0, 0, 50, 100))
+--Size Left Half
+hs.hotkey.bind(windowGridKeyCombo, 'L', function() gridset(0, 0, 50, 100) end)
 -- Size Right Half
-hs.hotkey.bind(windowGridKeyCombo, 'R', gridset(50, 0, 50, 100))
+hs.hotkey.bind(windowGridKeyCombo, 'R', function() gridset(50, 0, 50, 100) end)
 -- Size Full
-hs.hotkey.bind(windowGridKeyCombo, 'F', gridset(0, 0, 100, 100))
+hs.hotkey.bind(windowGridKeyCombo, 'F', function() gridset(0, 0, 100, 100) end)
 -- Size Centered
-hs.hotkey.bind(windowGridKeyCombo, 'C', gridset(12.5, 12.5, 75, 75))
+hs.hotkey.bind(windowGridKeyCombo, 'C', function() gridset(12.5, 12.5, 75, 75) end)
 -- Size Left 3/4ths
-hs.hotkey.bind(windowGridKeyCombo, 'N', gridset(0, 0, 75, 100, 'three-quarters'))
+hs.hotkey.bind(windowGridKeyCombo, 'N', function() gridset(0, 0, 75, 100, 'three-quarters') end)
 -- Size 3/4ths Centered
-hs.hotkey.bind(windowGridKeyCombo, 'X', gridset(12.5, 0, 75, 100))
+hs.hotkey.bind(windowGridKeyCombo, 'X', function() gridset(12.5, 0, 75, 100) end)
 -- Size Right 1/4th
-hs.hotkey.bind(windowGridKeyCombo, 'M', gridset(75, 0, 25, 100, 'one-quarter'))
+hs.hotkey.bind(windowGridKeyCombo, 'M', function() gridset(75, 0, 25, 100, 'one-quarter') end)
 -- Size Right 1/4th Top 1/2-ish
-hs.hotkey.bind(windowGridKeyCombo, ',', gridset(75, 0, 25, 55))
+hs.hotkey.bind(windowGridKeyCombo, ',', function() gridset(75, 0, 25, 55) end)
 -- Size Right 1/4th Bottom 1/2-ish
-hs.hotkey.bind(windowGridKeyCombo, '.', gridset(75, 60, 25, 40))
+hs.hotkey.bind(windowGridKeyCombo, '.', function() gridset(75, 60, 25, 40) end)
 -- Size Half Height, Top Edge
-hs.hotkey.bind(windowGridKeyCombo, 'T', gridset('current', 0, 'current', 50))
+hs.hotkey.bind(windowGridKeyCombo, 'T', function() gridset('current', 0, 'current', 50) end)
 -- Size Half Height, Bottom Edge
-hs.hotkey.bind(windowGridKeyCombo, 'B', gridset('current', 50, 'current', 50))
+hs.hotkey.bind(windowGridKeyCombo, 'B', function() gridset('current', 50, 'current', 50) end)
 -- Move to Left Edge
-hs.hotkey.bind(windowGridKeyCombo, ';', gridset(0, 'current', 'current', 'current'))
+hs.hotkey.bind(windowGridKeyCombo, ';', function() gridset(0, 'current', 'current', 'current') end)
 -- Move to Right Edge
-hs.hotkey.bind(windowGridKeyCombo, "'", gridset('opp', 'current', 'current', 'current'))
+hs.hotkey.bind(windowGridKeyCombo, "'", function() gridset('opp', 'current', 'current', 'current') end)
+
+hs.application.watcher.new(function(name, event, app)
+  if name == 'Terminal' and event == hs.application.watcher.launched then
+    gridset(75, 0, 25, 100, 'one-quarter')
+  elseif name == 'TextEdit' and event == hs.application.watcher.launched then
+    gridset(75, 60, 25, 40)
+  elseif (name == 'Atom' or name == 'GitHub Desktop') and event == hs.application.watcher.launched then
+    gridset(0, 0, 75, 100, 'three-quarters')
+  elseif name == 'Google Chrome' and event == hs.application.watcher.launched then
+    hs.timer.doAfter(0.25, function()
+      gridset(0, 0, 75, 100, 'three-quarters')
+      hs.timer.doAfter(0.25, function()
+        gridset(0, 'current', 'current', 'current')
+      end)
+    end)
+  end
+end):start()
