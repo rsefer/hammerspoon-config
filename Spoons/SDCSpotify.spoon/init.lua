5ce4a37bf974b26859d07ef6e08bc357fdd46fc6
Robert Sefer <rsefer@gmail.com>
2018-02-25 16:40:36 -0600

    Added progress bar

--- Spoons/SDCSpotify.spoon/init.lua
+++ Spoons/SDCSpotify.spoon/init.lua
@@ -7,7 +7,7 @@ function songString(artist, track)
   return artist .. ' - ' .. '"' .. track .. '"'
 end
 
-function obj:setSpotifyMenusText()
+function obj:setSpotifyMenus()
   if obj.spotifyControlMenu then
     if hs.spotify:isPlaying() then
       obj.spotifyControlMenu:setTitle("❚❚")
@@ -18,7 +18,10 @@ function obj:setSpotifyMenusText()
   if obj.spotifyTitleMenu and hs.spotify.getCurrentArtist() and hs.spotify.getCurrentTrack() then
     newSongString = songString(hs.spotify.getCurrentArtist(), hs.spotify.getCurrentTrack())
     obj.spotifyTitleMenu:setTitle(newSongString)
+    obj.currentSongPosition = hs.spotify.getPosition()
+
     if newSongString ~= obj.currentSong then
+      obj.currentSongDuration = hs.spotify.getDuration()
       hs.alert.closeSpecific(obj.currentSongAlertUUID, 0)
       obj.currentSongAlertUUID = hs.alert.show("🎵 " .. newSongString, {
         fillColor = {
@@ -39,14 +42,38 @@ function obj:setSpotifyMenusText()
         atScreenEdge = 1
       }, 5)
     end
+
+    if obj.showCurrentSongProgressBar then
+      currentSongPositionPercentage = obj.currentSongPosition / obj.currentSongDuration
+
+      obj.spotifyTitleMenuFrame = obj.spotifyTitleMenu:frame()
+      if obj.currentSongProgressBar then
+        obj.currentSongProgressBar:delete()
+      end
+      obj.currentSongProgressBar = hs.drawing.line({
+        x = obj.spotifyTitleMenuFrame.x,
+        y = obj.spotifyTitleMenuFrame.h - 2
+      },
+      {
+        x = obj.spotifyTitleMenuFrame.x + (obj.spotifyTitleMenuFrame.w * currentSongPositionPercentage),
+        y = obj.spotifyTitleMenuFrame.h - 2
+      })
+      obj.currentSongProgressBar:setStrokeColor({['hex'] = '1db954', ['alpha'] = 1})
+      obj.currentSongProgressBar:setStrokeWidth(4)
+      if currentSongPositionPercentage > 0.05 then
+        -- helps when song title length changes
+        obj.currentSongProgressBar:show()
+      end
+    end
+
     obj.currentSong = newSongString
   end
 end
 
 function obj:loadSpotifyMenus()
-  obj.spotifyTitleMenu = hs.menubar.new():setClickCallback(obj.openSpotify)
+  obj.spotifyTitleMenu = hs.menubar.new():setClickCallback(function() hs.application.launchOrFocus('Spotify') end)
   obj.spotifyControlMenu = hs.menubar.new():setClickCallback(obj.spotifyTogglePlayPause)
-  obj:setSpotifyMenusText()
+  obj:setSpotifyMenus()
 end
 
 function obj:unloadSpotifyMenus()
@@ -60,27 +87,26 @@ end
 
 function obj:spotifyTogglePlayPause()
   hs.spotify.playpause()
-  obj:setSpotifyMenusText()
-end
-
-function obj:openSpotify()
-  hs.application.launchOrFocus('Spotify')
+  obj:setSpotifyMenus()
 end
 
 function obj:init()
   if hs.spotify.isRunning() then
     self:loadSpotifyMenus()
   end
+  self.showCurrentSongProgressBar = true
   self.currentSong = ''
   self.spotifyTimer = hs.timer.doEvery(2, function()
     if hs.spotify:isRunning() then
-      obj:setSpotifyMenusText()
+      obj:setSpotifyMenus()
     end
   end):stop()
 end
 
 function obj:start()
 
+  obj.currentSongDuration = 0
+  obj.currentSongPosition = 0
   obj.spotifyTimer:start()
 
   obj.watcher = hs.application.watcher.new(function(name, event, app)
