f14e55f81e421d8fc03d78418cd1c1f39429fd84
Robert Sefer <rsefer@gmail.com>
2018-02-26 23:03:23 -0600

    Vastly improved spotify progress bar

--- Spoons/SDCSpotify.spoon/init.lua
+++ Spoons/SDCSpotify.spoon/init.lua
@@ -7,6 +7,11 @@ function songString(artist, track)
   return artist .. ' - ' .. '"' .. track .. '"'
 end
 
+function round(num, numDecimalPlaces)
+  local mult = 10^(numDecimalPlaces or 0)
+  return math.floor(num * mult + 0.5) / mult
+end
+
 function obj:setSpotifyMenus()
   if obj.spotifyControlMenu then
     if hs.spotify:isPlaying() then
@@ -17,7 +22,6 @@ function obj:setSpotifyMenus()
   end
   if obj.spotifyTitleMenu and hs.spotify.getCurrentArtist() and hs.spotify.getCurrentTrack() then
     newSongString = songString(hs.spotify.getCurrentArtist(), hs.spotify.getCurrentTrack())
-    obj.spotifyTitleMenu:setTitle(newSongString)
     obj.currentSongPosition = hs.spotify.getPosition()
 
     if newSongString ~= obj.currentSong then
@@ -58,29 +62,34 @@ function obj:setSpotifyMenus()
     if obj.showCurrentSongProgressBar then
       currentSongPositionPercentage = obj.currentSongPosition / obj.currentSongDuration
 
-      obj.spotifyTitleMenuFrame = obj.spotifyTitleMenu:frame()
-      if obj.currentSongProgressBar then
-        obj.currentSongProgressBar:delete()
-      end
-      obj.currentSongProgressBar = hs.drawing.line({
-        x = obj.spotifyTitleMenuFrame.x,
-        y = obj.spotifyTitleMenuFrame.h - 2
-      },
-      {
-        x = obj.spotifyTitleMenuFrame.x + (obj.spotifyTitleMenuFrame.w * currentSongPositionPercentage),
-        y = obj.spotifyTitleMenuFrame.h - 2
-      })
-      alpha = 1
-      -- if currentSongPositionPercentage < 0.1 then
-      --   alpha = currentSongPositionPercentage * 10
-      -- end
-      obj.currentSongProgressBar:setStrokeColor({['hex'] = '1db954', ['alpha'] = alpha})
-      obj.currentSongProgressBar:setStrokeWidth(4)
-      -- obj.currentSongProgressBar:show()
-      if currentSongPositionPercentage > 0.05 then
-        -- helps when song title length changes
-        obj.currentSongProgressBar:show()
-      end
+      fontCharacterWidth = 7 -- with SF Mono @ size 14, use 7 or 8
+
+      menubarHeight = 22
+
+      obj.menubarCanvas = hs.canvas.new({ x = 0, y = 0, h = menubarHeight, w = (string.len(newSongString)) * fontCharacterWidth })
+        :appendElements({
+          id = 'songText',
+          type = 'rectangle',
+          action = 'fill',
+          frame = {
+            x = '0%',
+            y = menubarHeight - 2,
+            h = 2,
+            w = round(currentSongPositionPercentage * 100, 2) .. '%'
+          },
+          fillColor = { ['hex'] = '1db954', ['alpha'] = 1.0 }
+        },
+        {
+          id = 'songProgress',
+          type = 'text',
+          text = newSongString,
+          textSize = 14,
+          textLineBreak = 'truncateTail',
+          textColor = { black = 1.0 },
+          frame = { x = '0%', y = 1, h = '100%', w = '100%' }
+        })
+
+      obj.spotifyTitleMenu:setIcon(obj.menubarCanvas:imageFromCanvas(), false)
     end
 
     obj.currentSong = newSongString
