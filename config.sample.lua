f91b17d15a1f485b0ede1c5905aff1207aab963e
Robert Sefer <rsefer@gmail.com>
2019-10-10 14:08:39 -0500

    Added sample config

---
+++ config.sample.lua
@@ -0,0 +1,18 @@
+keys = {
+	homeassistant_api_endpoint = '',
+	homeassistant_api_key = '',
+	dashHomeURL = '',
+	darksky_api_key = '',
+	latitude = '',
+	longitude = '',
+	phoneNumbers = {
+		{
+			text = '[name]',
+			number = '1234567890'
+		}
+	},
+	discogs = {
+		key = '',
+		secret = ''
+	}
+}
