570166dc35b3af01f0ce0395b3b7d049f6249c84
Robert Sefer <rsefer@gmail.com>
2019-10-23 15:09:46 -0500

    Switched back to Yam

--- lib/settings.lua
+++ lib/settings.lua
@@ -1,6 +1,7 @@
 hs.settings.set('hotkeyCombo', {'cmd', 'alt', 'ctrl'})
 hs.settings.set('secondaryMonitorName', 'DELL P2415Q')
-hs.settings.set('tertiaryMonitorName', 4128836) -- Duet doesn't have a name so we use the ID
+-- hs.settings.set('tertiaryMonitorName', 4128836) -- Duet doesn't have a name so we use the ID
+hs.settings.set('tertiaryMonitorName', 'Yam Display')
 hs.settings.set('screenClass', 'large') -- assumes large iMac
 if string.match(string.lower(hs.host.localizedName()), 'macbook') then
   hs.settings.set('screenClass', 'small')
