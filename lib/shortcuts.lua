e1fc354f4b4f2ac53959e494253a36a9278155e9
Robert Sefer <rsefer@gmail.com>
2019-11-16 12:18:00 -0600

    Google suggestions

--- lib/shortcuts.lua
+++ lib/shortcuts.lua
@@ -11,6 +11,33 @@ hs.hotkey.bind(hs.settings.get('hotkeyCombo'), '/', function() hs.reload() end)
 -- The above are not included in the Hammerspoon config because they won't
 -- work if Hammerspoon is not running (launch) or frozen (force quit)
 
+-- Google Query Suggestions
+-- Based heavily on Andrew Hampton's "autocomplete"
+-- https://github.com/andrewhampton/dotfiles/blob/8136fafe8aabee49f8cea0ab3da6c9e7be472e62/hammerspoon/.hammerspoon/anycomplete.lua
+hs.hotkey.bind(hs.settings.get('hotkeyCombo'), 'G', function()
+	local GOOGLE_ENDPOINT = 'https://suggestqueries.google.com/complete/search?client=chrome&num=5&q=%s'
+	local current = hs.application.frontmostApplication()
+	local chooser = hs.chooser.new(function(choice)
+		if not choice then return end
+		current:activate()
+		hs.eventtap.keyStrokes(choice.text)
+	end)
+	chooser:queryChangedCallback(function(string)
+		local query = hs.http.encodeForQuery(string)
+		hs.http.asyncGet(string.format(GOOGLE_ENDPOINT, query), nil, function(status, data)
+			if not data then return end
+			local ok, results = pcall(function() return hs.json.decode(data) end)
+			if not ok then return end
+			choices = hs.fnutils.imap(results[2], function(result)
+				return { ['text'] = result }
+			end)
+			chooser:choices(choices)
+		end)
+	end)
+	chooser:searchSubText(false)
+	chooser:show()
+end)
+
 -- New Google Calendar Event
 hs.hotkey.bind(hs.settings.get('hotkeyCombo'), '8', function()
 	hs.urlevent.openURL('https://calendar.google.com/calendar/r/eventedit')
