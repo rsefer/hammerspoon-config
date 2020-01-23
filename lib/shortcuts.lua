2bbe41ca789f49c0eb1e12583f7b2f9b8da07c9b
Robert Sefer <rsefer@gmail.com>
2020-01-22 21:20:24 -0600

    Better location

--- lib/shortcuts.lua
+++ lib/shortcuts.lua
@@ -17,9 +17,9 @@ if hs.location.servicesEnabled() and hs.location.authorizationStatus() == 'autho
 	hs.settings.set('latitude', location.latitude)
 	hs.settings.set('longitude', location.longitude)
 	hs.location.register('updateLocationTag', function(locationTable)
-		hs.settings.set('latitude', location.latitude)
-		hs.settings.set('longitude', location.longitude)
-	end)
+		hs.settings.set('latitude', locationTable.latitude)
+		hs.settings.set('longitude', locationTable.longitude)
+	end, 400)
 else
 	hs.alert.show('⛅️Cannot retrieve lat/lng')
 end
