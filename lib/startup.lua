4ebedf285c8a1deb65eef19a2c8d7d885d72ef1c
Robert Sefer <rsefer@gmail.com>
2020-05-13 11:01:39 -0500

    Brevity

--- lib/startup.lua
+++ lib/startup.lua
@@ -5,16 +5,11 @@ hs.alert.defaultStyle.textSize = 40
 
 -- clears Hammerspoon error notifications
 hs.osascript.applescript([[
-	tell application "System Events"
-		tell process "NotificationCenter"
-			set windowCount to count windows
-			repeat with i from windowCount to 1 by -1
-				if value of static text 1 of window i is "Hammerspoon error" then
-					click button "Close" of window i
-				end if
-			end repeat
-		end tell
-	end tell
+tell application "System Events" to tell process "NotificationCenter"
+	repeat with thisWindow in windows
+		if value of static text 1 of thisWindow is "Hammerspoon error" then click button "Close" of thisWindow
+	end repeat
+end tell
 ]])
 
 if hs.updateAvailable() ~= false then
