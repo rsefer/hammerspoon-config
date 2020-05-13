17e002b1807ae260a96a2578ba32c91a919c93f5
Robert Sefer <rsefer@gmail.com>
2020-05-13 12:01:50 -0500

    Much easier clear notifications

--- lib/startup.lua
+++ lib/startup.lua
@@ -1,16 +1,8 @@
+hs.notify.withdrawAll()
 hs.location.start()
 hs.window.filter.setLogLevel(1)
 hs.hotkey.setLogLevel(1)
 hs.alert.defaultStyle.textSize = 40
-
--- clears Hammerspoon error notifications
-hs.osascript.applescript([[
-tell application "System Events" to tell process "NotificationCenter"
-	repeat with thisWindow in windows
-		if value of static text 1 of thisWindow is "Hammerspoon error" then click button "Close" of thisWindow
-	end repeat
-end tell
-]])
 
 if hs.updateAvailable() ~= false then
 	hs.alert.show('Hammerspoon update available: ' .. hs.updateAvailable())
