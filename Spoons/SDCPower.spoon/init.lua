d107be143d2232cda485d0f72c898b0cb312847b
Robert Sefer <rsefer@gmail.com>
2020-05-12 19:57:45 -0500

    Battery drawing

--- Spoons/SDCPower.spoon/init.lua
+++ Spoons/SDCPower.spoon/init.lua
@@ -9,7 +9,7 @@ function obj:updateBatteryMenu()
 	batteryTooltipString = ''
 	if hs.battery.powerSource() == 'AC Power' then
 		if hs.battery.isCharged() then
-			obj.batteryMenu:setTitle(nil)
+			obj.batteryMenu:setIcon(nil):setTitle(nil)
 			return
 		end
 		batteryTooltipString = 'Charging'
@@ -21,13 +21,84 @@ function obj:updateBatteryMenu()
 	end
 	batteryDisplayString = batteryDisplayString .. math.floor(hs.battery.percentage()) .. '%'
 	if hs.battery.powerSource() ~= 'AC Power' then
-		batteryDisplayIcon = '🔋 '
+		batteryDisplayIcon = ' '
 		if hs.battery.timeRemaining() > 0 then
 			batteryDisplayString = batteryDisplayString .. ' ' .. minutesToClock(hs.battery.timeRemaining())
 		end
+		obj.batteryMenu:setIcon(asciiBattery(hs.battery.percentage()), false)
 	end
 	obj.batteryMenu:setTitle(hs.styledtext.new(batteryDisplayIcon, { font = { size = 12 } }) .. hs.styledtext.new(batteryDisplayString, { font = { name = 'SF Mono', size = 12 } }))
 	obj.batteryMenu:setTooltip(batteryTooltipString)
+end
+
+function asciiBattery(batteryPercentage)
+	-- example:
+	-- ....................
+	-- 1==================4
+	-- =a=============d...=
+	-- ==.............=...=
+	-- ==.............=...=
+	-- ==.............=...=
+	-- ==.............=...=
+	-- =b=============c...=
+	-- 2==================3
+	-- ....................
+
+	totalInnerRows = 6
+	totalCols = 20
+
+	lastFilledCol = math.ceil(batteryPercentage / 100 * (totalCols - 2))
+
+	asciiString = ''
+	asciiString = asciiString .. '....................' .. "\n"
+	asciiString = asciiString .. '1==================4' .. "\n"
+	for r = 1, totalInnerRows do
+		for c = 1, totalCols do
+			if c == 1 or c == totalCols then
+				asciiString = asciiString .. '='
+			else
+				if c == 2 then
+					if r == 1 then
+						asciiString = asciiString .. 'a'
+					elseif r == totalInnerRows then
+						asciiString = asciiString .. 'b'
+					else
+						asciiString = asciiString .. '='
+					end
+				elseif c == lastFilledCol then
+					if r == 1 then
+						asciiString = asciiString .. 'd'
+					elseif r == totalInnerRows then
+						asciiString = asciiString .. 'c'
+					else
+						asciiString = asciiString .. '='
+					end
+				elseif c ~= lastFilledCol then
+					asciiString = asciiString .. '.'
+				end
+			end
+		end
+		asciiString = asciiString .. "\n"
+	end
+	asciiString = asciiString .. '2==================3' .. "\n"
+	asciiString = asciiString .. '....................' .. "\n"
+
+	batteryOutlineStrokeColor = { red = 0, green = 0, blue = 0 }
+	if hs.host.interfaceStyle() == 'Dark' then
+		batteryOutlineStrokeColor = { red = 1, green = 1, blue = 1 }
+	end
+
+	batteryFillColor = { green = 1 }
+	if batteryPercentage < 25 then
+		batteryFillColor = { red = 1 }
+	elseif batteryPercentage < 60 then
+		batteryFillColor = { red = 1, green = 1 }
+	end
+
+	return hs.image.imageFromASCII(asciiString, {
+		{ strokeColor = batteryOutlineStrokeColor, fillColor = { alpha = 0 } },
+		{ strokeColor = { alpha = 0 }, fillColor = batteryFillColor}
+	})
 end
 
 function obj:init()
