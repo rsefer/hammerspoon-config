2d5b469844cd1ed65192bf07b6fb9e112cbef0e5
Robert Sefer <rsefer@gmail.com>
2020-05-29 10:03:24 -0500

    Fixed chooser issues

--- Spoons/SDCPasteboard.spoon/init.lua
+++ Spoons/SDCPasteboard.spoon/init.lua
@@ -3,13 +3,29 @@ local obj = {}
 obj.__index = obj
 obj.name = "SDCPasteboard"
 
+function obj:clearHistory()
+	obj.pasteboardHistory = {}
+	hs.settings.set('pasteboardHistory', obj.pasteboardHistory)
+end
+
 function obj:storeCopy()
 	now = hs.pasteboard.changeCount()
 	if now > obj.lastPasteboardChange then
-		currentClipboard = hs.pasteboard.getContents()
+		currentClipboard = hs.pasteboard.getContents() or ''
+		icon = '📄'
+		typesAvailable = hs.pasteboard.typesAvailable()
+		if string.sub(currentClipboard, 1, 4) == 'http' or typesAvailable['URL'] ~= nil then
+			icon = '🔗'
+		elseif typesAvailable['image'] ~= nil then
+			icon = '🖼️'
+		elseif typesAvailable['styledText'] ~= nil then
+			icon = '📝'
+		end
 		table.insert(obj.pasteboardHistory, 1, {
 			content = currentClipboard,
-			timestamp = os.time()
+			timestamp = os.time(),
+			contentTypes = hs.pasteboard.allContentTypes()[1],
+			icon = icon
 		})
 		while (tablelength(obj.pasteboardHistory) >= 10) do
 			table.remove(obj.pasteboardHistory, tablelength(obj.pasteboardHistory))
@@ -19,23 +35,23 @@ function obj:storeCopy()
 	end
 end
 
-function obj:populatePasteboardChooser()
-	if tablelength(hs.settings.get('pasteboardHistory')) == 0 then return end
-
+function obj:populateChooser()
 	titleMaxLength = 70
 	ellipsesString = ' [...]'
 	choices = {}
-	for k, item in pairs(hs.settings.get('pasteboardHistory')) do
-		title = item.content
-		if string.len(item.content) > titleMaxLength then
-			title = title:gsub("\n", ""):gsub("\r", ""):gsub("\t", "")
-			title = string.sub(title, 1, titleMaxLength - string.len(ellipsesString)) .. ellipsesString
+	if tablelength(hs.settings.get('pasteboardHistory')) > 0 then
+		for k, item in pairs(hs.settings.get('pasteboardHistory')) do
+			title = item.icon .. ' ' .. item.content
+			if string.len(item.content) > titleMaxLength then
+				title = title:gsub("\n", ""):gsub("\r", ""):gsub("\t", "")
+				title = string.sub(title, 1, titleMaxLength - string.len(ellipsesString)) .. ellipsesString
+			end
+			table.insert(choices, {
+				text = title,
+				subText = os.date('%I:%M%p', item.timestamp),
+				fullText = item.content
+			})
 		end
-		table.insert(choices, {
-			text = title,
-			subText = os.date('%I:%M%p', item.timestamp),
-			fullText = item.content
-		})
 	end
 	obj.chooser:rows(tablelength(choices) + 1)
 		:choices(choices)
@@ -80,19 +96,16 @@ function obj:init()
 			id = 'pasteboardClear',
 			label = 'Clear',
 			selectable = true,
-			fn = function()
-				obj.pasteboardHistory = {}
-				hs.settings.set('pasteboardHistory', obj.pasteboardHistory)
-			end
+			fn = obj.clearHistory
 		}}):sizeMode('small'):displayMode('label'))
 		:width(self.chooserWidthPercentage)
 
 	self.timer = hs.timer.new(1, self.storeCopy)
 
-	self:populatePasteboardChooser()
+	self:populateChooser()
 
 	hs.settings.watchKey('settings_pasteboardHistory_watcher', 'pasteboardHistory', function()
-		obj:populatePasteboardChooser()
+		obj:populateChooser()
 	end)
 
 end
