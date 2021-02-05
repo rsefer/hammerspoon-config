0efafcba19d6bc4c05f06beb72fe311ad1c31a21
Robert Sefer <rsefer@gmail.com>
2021-02-05 13:02:13 -0600

    Added basecamp

--- lib/spoons.lua
+++ lib/spoons.lua
@@ -6,7 +6,7 @@ hs.spoons.use('SDCWindows', {
 	config = {
 		windowLayout = {
 			{
-				apps = { 'Google Chrome', 'Brave Browser', 'Safari', 'Firefox', 'Music', 'Spotify', 'Photos', 'App Store', 'Coda', 'TV', 'News', 'Podcasts', 'Postman' },
+				apps = { 'Google Chrome', 'Brave Browser', 'Safari', 'Firefox', 'Music', 'Spotify', 'Photos', 'App Store', 'Coda', 'TV', 'News', 'Podcasts', 'Postman', 'Basecamp 3' },
 				screens = {
 					deskWithiPad = nil,
 					desk = nil,
