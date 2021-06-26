3538174d4d1ef163dfaa01ff8db5626383a3afc6
Robert Sefer <rsefer@gmail.com>
2021-06-26 13:00:57 -0500

    Cleanup

--- lib/icons.lua
+++ lib/icons.lua
@@ -1,9 +1,13 @@
+-- If providing a non-string, will assume hex code
 -- https://mathew-kurian.github.io/CharacterMap/
 -- starting at 5800
 -- hexCode = 0x1001D3 of UNI1001D3.LARGE
 -- /Library/Fonts/SF Pro
-function symbolToImage(hexCode, color)
-	local char = hs.styledtext.new(utf8.char(hexCode), {
+function textToImage(input, color)
+	if type(input) == 'number' then
+		input = utf8.char(input)
+	end
+	local styledInput = hs.styledtext.new(input, {
 		font = {
 			name = 'SF Pro',
 			size = 100
@@ -11,35 +15,15 @@ function symbolToImage(hexCode, color)
 		color = color
 	})
 	local canvas = hs.canvas.new({ x = 0, y = 0, h = 0, w = 0 })
-	canvas:size(canvas:minimumTextSize(char))
-	canvas[#canvas + 1] = {
-		type = 'text',
-		text = char
-	}
+	canvas:size(canvas:minimumTextSize(styledInput))
+		:appendElements({
+			type = 'text',
+			text = styledInput
+		})
 	return canvas:imageFromCanvas()
 end
 
-function textToImage(text)
-	local text = hs.styledtext.new(text, {
-		font = {
-			size = 100
-		}
-	})
-	local canvas = hs.canvas.new({ x = 0, y = 0, h = 0, w = 0 })
-	canvas:size(canvas:minimumTextSize(text))
-	canvas[#canvas + 1] = {
-		type = 'text',
-		text = text
-	}
-	return canvas:imageFromCanvas()
-end
-
-iconSize = {
-	w = hs.settings.get('menuIconSize'),
-	h = hs.settings.get('menuIconSize')
-}
-
-iconPause = symbolToImage(0x100285):setSize({ w = 24, h = 24 })
-iconPlay = symbolToImage(0x100284):setSize({ w = 18, h = 18 })
-iconTimerOff = symbolToImage(0x10042F):setSize({ w = 18, h = 18 })
-iconTimerOn = symbolToImage(0x100430, { hex = '#1db954' }):setSize({ w = 18, h = 18 })
+iconPause = textToImage(0x100285):setSize({ w = 24, h = 24 })
+iconPlay = textToImage(0x100284):setSize({ w = 18, h = 18 })
+iconTimerOff = textToImage(0x10042F):setSize({ w = 18, h = 18 })
+iconTimerOn = textToImage(0x100430, { hex = '#1db954' }):setSize({ w = 18, h = 18 })
