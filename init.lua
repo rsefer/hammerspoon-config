99f458579981b780428d268702db0a1dca258b8d
Robert Sefer <rsefer@gmail.com>
2022-05-08 19:27:35 -0500

    Wedding over!

--- init.lua
+++ init.lua
@@ -11,22 +11,6 @@ require('lib/startup')
 require('lib/settings')
 require('lib/icons')
 
-local daysToWedding = 0
-daysTarget = os.time({ year = 2022, month = 5, day = 7, hour = 6 })
-daysMenubar = hs.menubar.new():setClickCallback(function()
-	hs.execute('say "yay! ' .. daysToWedding .. ' days to go" -r 200')
-end)
-function setDaysTitle()
-	daysToWedding = math.ceil(os.difftime(daysTarget, os.time()) / (24 * 60 * 60))
-	suffix = 's'
-	if daysToWedding == 1 then
-		suffix = ''
-	end
-	daysMenubar:setTitle(daysToWedding .. ' day' .. suffix .. ' 👰‍♀️🤵')
-end
-setDaysTitle()
-daysTimer = hs.timer.doEvery(60 * 1, setDaysTitle)
-
 require('lib/spoons')
 require('lib/shortcuts')
 require('lib/window-misc')
